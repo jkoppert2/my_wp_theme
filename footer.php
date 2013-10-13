37c28a54701dc55be8c1420db805ba64f0b625b7
cardiac-jack <jkoppert6@gmail.com>
2013-10-12 22:52:55 -0400

    Adding files

---
+++ footer.php
@@ -0,0 +1,15 @@
+<hr>
+
+      <footer>
+        <p>© Company 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap.js"></script>
+
+  </body>
+</html>
